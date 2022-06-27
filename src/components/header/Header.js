328768142e421352267fc486da71a5c0e34f2119
unknown <projects.mindiii@gmail.com>
2022-06-27 19:34:41 +0530

    working on home page design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,31 @@
 import React from "react";
-import { Navbar, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../../assets/images/white-logo.webp';
 import Advertisment from '../../assets/images/loudspeaker.webp';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "./Header.css";
 
+
+
 const Header = () =>{
     return(
         <>
            <div className="qv_home_wrapper">
-            <Navbar>
+            <Navbar expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">
-                        <img className="w-auto h-auto " src={Logo} />
-                    </Navbar.Brand>
+                    <Navbar.Brand href="#home"><img className="w-auto h-auto " src={Logo} /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto navMenu">
-                        <Nav.Link className="active" exact to="/" href="">Home</Nav.Link>
-                        <Nav.Link href="">MEET Qvaring</Nav.Link>
-                        <Nav.Link href="">Sign IN</Nav.Link>
+                    <Nav.Link className="active" exact to="/" href="">Home</Nav.Link>
+                                    <Nav.Link href="">MEET Qvaring</Nav.Link>
+                                    <Nav.Link href="">Sign IN</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar> 
+            </Navbar>
             <div class="qvAdvertisment">
                 <span><img src={Advertisment} /></span>
                 <marquee>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,57 @@
 import React from "react";
+import { BsInfoCircleFill } from 'react-icons/bs';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Button from 'react-bootstrap/Button';
+import "./Home.css";
 
 const Home = () =>{
     return(
         <>
-            <h1>jhkjklkjkklhkjljlk</h1>
+            <section className="IndexPageBG">
+                <div className="homeRchrgTypeSec">
+                    <div class="qv_banner_text">
+                        <h2>iTriple Saldo Principal!</h2>
+                        <p>Por cada recarga de 500 CUP Reciben 1500 CUP de saldo principal</p>
+                    </div>
+                    <div class="qv_operator_section qvIndexSelectRechType">
+                        <div class="form-group radioGroup sendType rechargeType">
+                            <label class="radioPaymnt">
+                                <input type="radio" name="transferClientType" value="1" class="required"/>
+                                <div class="recharge_type">
+                                    <p>Recharge <span>Recommended</span></p>
+                                    <h5>Super Saving
+                                    <OverlayTrigger
+                                        placement="top"
+                                        overlay={<Tooltip id="button-tooltip-2">Check out this avatar</Tooltip>}
+                                        >
+                                        {({ ref, ...triggerHandler }) => (
+                                            <sup
+                                            variant="light"
+                                            {...triggerHandler}
+                                            className="d-inline-flex align-items-center"
+                                            >
+
+                                            <BsInfoCircleFill />
+                                            </sup>
+                                        )}
+                                    </OverlayTrigger>
+                                </h5>
+                                </div>
+                            </label>
+                            <label class="radioPaymnt">
+                                <input type="radio" name="transferClientType" value="2" class="required" />
+                                <div class="recharge_type">
+                                    <p>Recharge</p>
+                                    <h5>Economy <sup class="info_tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="Save 40%. Estimated arrival time 1hr or less"><BsInfoCircleFill /></sup></h5>
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </section>
         </>
     );
 };
